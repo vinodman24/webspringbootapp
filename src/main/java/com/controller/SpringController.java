f03f68b46e77c1165f1de481a083267c7af9374d
vinodman24 <vinodhcl1920@gmail.com>
2024-06-25 17:45:31 +0530

    openapi

--- src/main/java/com/controller/SpringController.java
+++ src/main/java/com/controller/SpringController.java
@@ -1,88 +1,56 @@
 package com.controller;
 
-import com.model.User;
-import com.repository.UserRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import javax.persistence.EntityNotFoundException;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(description = "User API", name = "User Services")
 @Controller
-public class SpringController {
+public class SpringController 
+{
+@GetMapping(path="/hello-world")
+public String helloWorld()
+{
+	try { 
+		 TimeUnit.SECONDS.sleep(45); 
+		 } catch (InterruptedException e) {
+	 System.out.println("Thread interrupted"); 
+	 }
+return "Response after sleep";
+}
 
-    private final AtomicInteger counter = new AtomicInteger();
+@GetMapping("/users")
+@Operation(summary = "Returns all users", tags = {"User",},
+        responses = {
+                @ApiResponse(responseCode = "200",
+                        description = "Returns all users",
+                        content = @Content(mediaType = "application/json",
+                                schema = @Schema(implementation = User.class)))
+        })
+@ResponseBody
+public List<User> getAllUsers() {
+	
+	 try { 
+		 TimeUnit.SECONDS.sleep(45); 
+		 } catch (InterruptedException e) {
+	 System.out.println("Thread interrupted"); 
+	 }
+	 List<User> users = new ArrayList<>();
+     users.add(new User(1, "John Doe"));
+     users.add(new User(2, "Dow"));
+     return users;
+	 
+}
 
-    private final UserRepository userRepository;
-
-    @Autowired
-    public SpringController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @GetMapping("/users")
-    @Operation(summary = "Returns all users", tags = {"User",},
-            responses = {
-                    @ApiResponse(responseCode = "200",
-                            description = "Returns all users",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = User.class)))
-            })
-    @ResponseBody
-    public List<User> getAllUsers() {
-         try { 
-			 TimeUnit.SECONDS.sleep(45); 
-			 } catch (InterruptedException e) {
-		 System.out.println("Thread interrupted"); 
-		 }
-        return userRepository.findAll();
-    }
-
-    @PostMapping("/users")
-    @Operation(summary = "Register a new user",
-            responses = {
-                    @ApiResponse(responseCode = "200",
-                            description = "Returns the registered user",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = User.class)))
-            })
-    @ResponseBody
-    public User register(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
-        User newUser = new User(counter.incrementAndGet(), name);
-        return userRepository.addUser(newUser);
-    }
-
-    @PutMapping("/users/{id}")
-    @Operation(summary = "Update a user's name",
-            responses = {
-                    @ApiResponse(responseCode = "200",
-                            description = "Returns the updated user",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = User.class)))
-            })
-    @ResponseBody
-    public User updateUser(@PathVariable(value = "id") int id, String newName) {
-        return userRepository.updateUser(id, newName).orElseThrow(() -> new EntityNotFoundException("Error! User not found!"));
-    }
-
-    @DeleteMapping("/users/{id}")
-    @Operation(summary = "Delete a user",
-            responses = {
-                    @ApiResponse(responseCode = "200",
-                            description = "Returns a boolean",
-                            content = @Content(mediaType = "application/json"))
-            })
-    @ResponseBody
-    public Boolean deleteUser(@PathVariable(value = "id") int id) {
-        return userRepository.deleteUser(id);
-    }
 }
